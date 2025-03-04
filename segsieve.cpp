c548a0826a3fb64cba4c9ddd594f27143885bc3d
Gregor Hartl Watters <mario.garwh@gmail.com>
2025-03-04 06:59:00 +0100

    Can't believe I forgot to commit! I wrote - maybe 3-4 days ago - a segmented Sieve of Eratosthenes. This provides an enormous increase in storage efficiency, from O(N) to O(sqrt(N)). In addition, the segmented sieve actually appears to also run slightly faster than the regular sieve - perhaps due to less memory being allocated.

--- segsieve.cpp
+++ segsieve.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cinttypes>
 #include <iostream>
 #include <concepts>
@@ -29,19 +30,32 @@ int main(int argc, char **argv) {
         return 1;
     }
     uint64_t N = to_uint(*(argv + 1));
+    if (N < 2)
+        return 0;
+    if (N == 2) {
+        std::cout << "2\n";
+        return 0;
+    }
+    if (N == 3) {
+        std::cout << "2\n3\n";
+        return 0;
+    }
     uint64_t i = 3;
     double broot = sqrt((double) N);
     uint64_t broot_u = broot;
-    std::vector<bool> marked((broot_u % 2 ? broot_u / 2 : broot_u / 2 - 1), false);
+    broot_u += broot_u % 2; // made sure the root-value is even as this avoids having to switch between odd/even sizes of "marked" in the loop
+    uint64_t size = broot_u/2;//broot_u % 2 ? broot_u / 2 : broot_u / 2 - 1;
+    std::vector<bool> marked(size, false);
     uint64_t multiple;
     std::cout << "2\n"; // only even prime
     uint64_t two_i;
     uint64_t idx = 0;
     uint64_t midx;
     std::vector<uint64_t> primes;
-    while (i <= broot_u) {
+    // std::cout << "broot: " << broot << ", broot_u: " << broot_u << ", marked.size(): " << marked.size() << std::endl;
+    do {
         multiple = i*i;
-        two_i = 2*i;
+        two_i = i << 1;
         midx = multiple/2 - 1;
         while (multiple <= broot_u) {
             marked[midx] = true; // marked as composite
@@ -51,32 +65,73 @@ int main(int argc, char **argv) {
         std::cout << i << '\n';
         primes.push_back(i);
         i += 2;
-        while (marked[++idx]) i += 2;
-    }
-    primes.push_back(i);
+        while (++idx < size && marked[idx]) i += 2;
+    } while (i <= broot_u);
+    // primes.push_back(i);
     uint64_t prev = broot_u + 1;
-    uint64_t next = 2*broot_u;
+    uint64_t next = broot_u << 1;
     uint64_t maxw = broot_u*broot_u;
-    std::vector<bool> window(broot_u, false);
-    uint64_t psq;
-    uint64_t diff;
+    maxw = maxw < N ? maxw : N;
+    // std::vector<bool> window(broot_u);
+    // uint64_t psq;
+    // uint64_t diff;
+    uint64_t p2;
+    // for (const auto &prime : primes)
+        // std::cout << "prime: " << prime << std::endl;
+    uint64_t rem;
+    // std::cout << "marked.size(): " << marked.size() << std::endl;
     while (next <= maxw) {
-        for (const auto uint64_t &prime : primes) {
-            psq = prime*prime;
-            i = prev + prime - (prev % prime);
-            i = i > psq ? i : psq;
-            diff = i - prev;
+        idx = 0;
+        while (idx < size) {
+            marked[idx++] = false;
+        }
+        for (const uint64_t &prime : primes) {
+            i = prev + ((rem = prev % prime) ? prime - rem : 0);
+            if (!(i % 2))
+                i += prime;
+            idx = (i - prev)/2;
+            p2 = prime << 1;
             while (i <= next) {
-                
+                /* if (idx == 49)
+                    std::cout << "Yes! idx: " << idx << ", i: " << i << ", prime: " << prime << std::endl; */
+                marked[idx] = true;
+                idx += prime;
+                i += p2;
             }
         }
-        i = prev;
-        while (i <= next) {
-            
+        i = prev + 1 - (prev % 2);
+        std::for_each(marked.begin(), marked.end(), [&i](bool b){if (!b) std::cout << i << '\n'; i += 2;});
+        /* for (bool b : marked) {
+            std::cout << std::boolalpha << "b: " << b << ", i: " << i << std::endl;
             i += 2;
-        }
+        } */
         prev = next + 1;
         next += broot_u;
+    }
+    if (prev > N)
+        return 0;
+    uint64_t left = N - prev;
+    left = left/2 + ((prev % 2) || (N % 2));
+    // std::cout << "left: " << left << ", N: " << N << ", prev: " << prev << std::endl;
+    if (left) {
+        marked.resize(left);
+        idx = left;
+        while (idx --> 0)
+            marked[idx] = false;
+        for (const uint64_t &prime : primes) {
+            i = prev + ((rem = prev % prime) ? prime - rem : 0);
+            if (!(i % 2))
+                i += prime;
+            idx = (i - prev)/2;
+            p2 = prime << 1;
+            while (i <= N) {
+                marked[idx] = true;
+                idx += prime;
+                i += p2;
+            }
+        }
+        i = prev % 2 ? prev : prev + 1;
+        std::for_each(marked.begin(), marked.end(), [&i](bool b){if (!b) std::cout << i << '\n'; i += 2;});
     }
     return 0;
 }
